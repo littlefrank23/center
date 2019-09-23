dfb7f100386abf3f17a630320b5d6c0396264e6b
Teng Yang <t.yang3141@gmail.com>
2019-09-23 22:20:39 +0800

    redis

--- src/test/java/com/mycom/center/CenterApplicationTests.java
+++ src/test/java/com/mycom/center/CenterApplicationTests.java
@@ -2,15 +2,31 @@ package com.mycom.center;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CenterApplicationTests {
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Test
+    public void redisTest(){
+        stringRedisTemplate.opsForValue().append("redis","redis_test");
+
+
+    }
+
     @Test
     public void contextLoads() {
+
     }
 
 }
